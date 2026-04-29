d87bbf574f01909d0717313e812c2956c0c77b29
leokumala21 <leokumala3120@gmail.com>
2026-04-29 16:53:34 +0700

    Add model classes for Category, Item, Order, OrderItem, Role, and User with relationships and soft deletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,24 +9,35 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $fillable = [
+        'username',
+        'password',
+        'full_name',
+        'email',
+        'phone',
+        'role_id',
+        'created_at',
+        'updated_at',
+
+    ];
+    protected $dates = ['deleted_at'];
+
+     public function role()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->belongsTo(Role::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
     }
 }
